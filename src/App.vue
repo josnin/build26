274b9999362f12598d312d71bc519ddab1ba4a27
Jose <nino.capati@gmail.com>
2022-02-01 22:37:27 +0800

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
-import { builder, RenderContent } from '@builder/vue';
 </script>
 
 <template>
