8ee58f62d1f6fbc29fb9b62994942163616161ce
Jose <nino.capati@gmail.com>
2022-02-02 21:52:10 +0800

    Box component first attempt

---
+++ src/components/Box.vue
@@ -0,0 +1,19 @@
+<script setup>
+  console.log('im  box')
+</script>
+
+<template>
+  <div> resize me??</div>
+</template>
+
+<style scoped>
+
+  div {
+    border: 2px solid #4286f4;
+    resize: both;
+    padding: 20px; 
+    width: 300px;
+    overflow: auto;
+  }
+
+</style>
