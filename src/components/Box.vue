fabca22142f5352f3216615040cbdf8d113bfd8b
Jose <nino.capati@gmail.com>
2022-02-05 21:51:54 +0800

    basic box drag & drop

--- src/components/Box.vue
+++ src/components/Box.vue
@@ -1,5 +1,6 @@
 <script setup>
   //import { defineProps } from 'vue';
+  import { ref, onMounted } from 'vue';
 
   const props = defineProps({
     justifyItems: {
@@ -20,18 +21,49 @@
     }
   })
 
+  const root = ref('');
+
+  onMounted(() => {
+    console.log('root value', root.value)
+  })
+
+
+  const onDrop = (e) => {
+    const data = e.dataTransfer.getData("text");
+    console.log('drop', e);
+    console.log('data here', data);
+    e.target.appendChild(root.value);
+  };
+
+  const onDrag = (e) => {
+    //e.dataTransfer.setData("text", e.target.id);
+    console.log('onDrag', root.value)
+  }
+
+
   console.log(`im  box ${props.width} ${props.height}`)
 </script>
 
 <template>
   <div 
+    ref="root"
     class="box" 
     :style="{ justifyItems: props.justifyItems, alignItems: props.alignItems }">
     <div 
+      draggable
+      @dragstart="onDrag($event)"
       class="box__size" 
       :style="{ width: props.width, height: props.height }"> 
         resize me??
     </div>
+  </div>
+
+  <div style="border:3px solid red;width:600px; height:600px" 
+    ref="root2"
+    @drop="onDrop($event)"
+    @dragover.prevent
+    >
+    Drop here
   </div>
 </template>
 
