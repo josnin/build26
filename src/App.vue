2a19205f826e8772f77ca3acb06e677d96247488
Jose <nino.capati@gmail.com>
2022-02-05 20:56:29 +0800

    enable alias dir in vite

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import Box from './components/Box.vue'
+import Box from '@/components/Box.vue'
 </script>
 
 <template>
