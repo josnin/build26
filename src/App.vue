b4f6d89ade2a186187b488106a51e83717ab08fa
Jose <nino.capati@gmail.com>
2022-11-27 19:28:52 +0800

    intro layout container

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import Canvas from '@/components/Canvas.vue'
+import Layout from '@/views/Layout.vue'
 </script>
 
 <template>
-  <Canvas/>
+  <Layout/>
 </template>
